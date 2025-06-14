37e25852a98b0d9ba065139c638bca31351211ab
Anil Ozsoy <anilozsoy@gmail.com>
2025-06-15 01:04:17 +0200

    feat(masterTemplate.tsx): add ToolBarButton components for selecting block types to enhance user interaction feat(Editor.tsx): implement zoom functionality with ToolBarButton and input for better canvas control fix(Editor.tsx): handle click outside zoom input to close the zoom interface for improved usability

--- app/components/master-template/masterTemplate.tsx
+++ app/components/master-template/masterTemplate.tsx
@@ -1,5 +1,8 @@
 import ToolBar from "../toolbar/toolBar";
+import ToolBarButton from "../toolbar/toolBarButton";
 import Frame from "./frame";
+import { RxText, RxTransform, RxImage } from "react-icons/rx";
+import { useEffect, useState } from "react";
 
 interface MasterTemplateProps {
   width: number;
@@ -7,9 +10,46 @@ interface MasterTemplateProps {
 }
 
 export default function MasterTemplate({ width, height }: MasterTemplateProps) {
+  const [selectedBlock, setSelectedBlock] = useState<string | null>(null);
+  useEffect(() => {
+    const pressEscape = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setSelectedBlock(null);
+      }
+    };
+    window.addEventListener("keydown", pressEscape);
+    return () => {
+      window.removeEventListener("keydown", pressEscape);
+    };
+  }, []);
   return (
     <div className="flex mt-4">
-      <ToolBar />
+      <ToolBar className="flex-col items-center gap-2 rounded-l-lg h-fit p-2">
+        <ToolBarButton
+          icon={<RxText className="text-xl" />}
+          onClick={() => {
+            setSelectedBlock("TextBlock");
+          }}
+          selectedBlock={selectedBlock}
+          blockType="TextBlock"
+        />
+        <ToolBarButton
+          icon={<RxTransform className="text-xl" />}
+          onClick={() => {
+            setSelectedBlock("ShapeBlock");
+          }}
+          selectedBlock={selectedBlock}
+          blockType="ShapeBlock"
+        />
+        <ToolBarButton
+          icon={<RxImage className="text-xl" />}
+          onClick={() => {
+            setSelectedBlock("ImageBlock");
+          }}
+          selectedBlock={selectedBlock}
+          blockType="ImageBlock"
+        />
+      </ToolBar>
       <Frame width={width} height={height} />
     </div>
   );
--- app/components/poster-editor/Editor.tsx
+++ app/components/poster-editor/Editor.tsx
@@ -1,8 +1,12 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { ImageBlock, ShapeBlock, TextBlock } from "@/app/types/poster";
 import MasterTemplate from "../master-template/masterTemplate";
 import Canvas from "../canvas/canvas";
+import ToolBar from "../toolbar/toolBar";
+import ToolBarButton from "../toolbar/toolBarButton";
+import { RxZoomIn } from "react-icons/rx";
+import { Input } from "@/components/ui/input";
 
 interface EditorProps {
   onSave?: (blocks: (TextBlock | ShapeBlock | ImageBlock)[]) => void;
@@ -12,11 +16,53 @@ export default function Editor({ onSave }: EditorProps) {
   const [blocks, setBlocks] = useState<(TextBlock | ShapeBlock | ImageBlock)[]>(
     []
   );
+  const [zoom, setZoom] = useState(50);
+  const [isZooming, setIsZooming] = useState(false);
+  const zoomRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (zoomRef.current && !zoomRef.current.contains(e.target as Node)) {
+        setIsZooming(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isZooming]);
 
   return (
     <div className="flex h-full w-full text-white ">
       <Canvas>
         <MasterTemplate width={600} height={900} />
+        <ToolBar className="flex justify-center absolute bottom-5 right-5 rounded-lg gap-4 h-fit p-2">
+          {!isZooming && (
+            <ToolBarButton
+              icon={<RxZoomIn className="text-xl" />}
+              onClick={() => {
+                setIsZooming(true);
+              }}
+              selectedBlock={null}
+              blockType="TextBlock"
+            />
+          )}
+          {isZooming && (
+            <div className="flex items-center gap-2" ref={zoomRef}>
+              <Input
+                type="number"
+                value={zoom}
+                min={0}
+                max={100}
+                onChange={(e) => setZoom(parseInt(e.target.value))}
+                className="h-7 text-center [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+              />
+              <span className="text-sm">%</span>
+            </div>
+          )}
+        </ToolBar>
       </Canvas>
     </div>
   );

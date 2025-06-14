602e6b13b8a11c1c0a657e6bdd1dffa9bce09933
Anil Ozsoy <anilozsoy@gmail.com>
2025-06-14 22:12:13 +0200

    feat(canvas): add Canvas component to manage selected blocks and cursor styles for a flexible editing experience

---
+++ app/components/canvas/canvas.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function Canvas({ children }: { children: React.ReactNode }) {
+  const [selectedBlock, setSelectedBlock] = useState<string | null>(null);
+
+  return (
+    <div className="flex-1">
+      <div
+        className={`flex flex-col items-center bg-neutral-900 w-full h-full ${
+          selectedBlock === "TextBlock"
+            ? "cursor-text"
+            : selectedBlock === "ShapeBlock"
+            ? "cursor-crosshair"
+            : selectedBlock === null
+            ? "cursor-default"
+            : selectedBlock === "ImageBlock"
+            ? "cursor-crosshair"
+            : "cursor-default"
+        }`}
+        id="editor-canvas"
+      >
+        {children}
+      </div>
+    </div>
+  );
+}

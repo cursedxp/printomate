4c8830f9d190474e6a9e83724872505faf27d1a7
Anil Ozsoy <anilozsoy@gmail.com>
2025-06-15 01:03:29 +0200

    fix(layout.tsx): add dark class to html element for improved theming support

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className="antialiased bg-neutral-900">{children}</body>
     </html>
   );

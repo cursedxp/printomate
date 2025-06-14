2b979c1d69bbff3b48e8e9ddfacc679111c005a1
Anil Ozsoy <anilozsoy@gmail.com>
2025-06-15 01:03:37 +0200

    refactor(toolbar.tsx): simplify ToolBar component by removing state and event listeners, and allow children to be passed for more flexibility

--- app/components/toolbar/toolBar.tsx
+++ app/components/toolbar/toolBar.tsx
@@ -1,49 +1,12 @@
-import { RxText, RxTransform, RxImage } from "react-icons/rx";
+interface ToolBarProps {
+  children: React.ReactNode;
+  className?: string;
+}
 
-import { useState, useEffect } from "react";
-import ToolBarButton from "./toolBarButton";
-
-export default function ToolBar() {
-  const [selectedBlock, setSelectedBlock] = useState<string | null>(null);
-
-  useEffect(() => {
-    const pressEscape = (e: KeyboardEvent) => {
-      if (e.key === "Escape") {
-        setSelectedBlock(null);
-      }
-    };
-    window.addEventListener("keydown", pressEscape);
-    return () => {
-      window.removeEventListener("keydown", pressEscape);
-    };
-  }, []);
-
+export default function ToolBar({ children, className }: ToolBarProps) {
   return (
-    <div className="flex flex-col items-center bg-neutral-900 gap-2 p-2 rounded-l-lg h-fit w-10 ">
-      <ToolBarButton
-        icon={<RxText className="text-xl" />}
-        onClick={() => {
-          setSelectedBlock("TextBlock");
-        }}
-        selectedBlock={selectedBlock}
-        blockType="TextBlock"
-      />
-      <ToolBarButton
-        icon={<RxTransform className="text-xl" />}
-        onClick={() => {
-          setSelectedBlock("ShapeBlock");
-        }}
-        selectedBlock={selectedBlock}
-        blockType="ShapeBlock"
-      />
-      <ToolBarButton
-        icon={<RxImage className="text-xl" />}
-        onClick={() => {
-          setSelectedBlock("ImageBlock");
-        }}
-        selectedBlock={selectedBlock}
-        blockType="ImageBlock"
-      />
+    <div className={`flex bg-neutral-800 items-center ${className}`}>
+      {children}
     </div>
   );
 }
